8409bcb66b157175281c7c0649edce023272bbc2
Aditya Raj <83997534+adityaraj607@users.noreply.github.com>
2026-04-28 08:20:55 +0530

    Coded UC3

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,25 +1,15 @@
+import java.util.Scanner;
+
 public class TicTacToe {
-    static char[][] board = new char[3][3];
+
     public static void main(String[] args) {
-        initializeBoard();
-        printBoard();
+        int slot = getUserSlot();
+        System.out.println("Slot entered: " + slot);
     }
-    static void initializeBoard() {
-        for (int row = 0; row < 3; row++) {
-            for (int col = 0; col < 3; col++) {
-                board[row][col] = '-';
-            }
-        }
-    }
-    static void printBoard() {
-        System.out.println("-------------");
-        for (int row = 0; row < 3; row++) {
-            System.out.print("| ");
-            for (int col = 0; col < 3; col++) {
-                System.out.print(board[row][col] + " | ");
-            }
-            System.out.println();
-            System.out.println("-------------");
-        }
+
+    static int getUserSlot() {
+        Scanner scanner = new Scanner(System.in);
+        int slot = scanner.nextInt();
+        return slot;
     }
 }
